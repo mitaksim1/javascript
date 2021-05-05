bf56d04e4e152f9104c7de37876cd0796081e956
mitaksim1 <miriamt.simonnet@gmail.com>
2021-05-05 14:50:15 +0200

    initial commit

---
+++ js/quotes.js
@@ -0,0 +1,74 @@
+var quotes = [
+    {
+        quote : "Dans le doute, reboot.",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "Il faut vider ton cache pour voir les modifications.",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "Normal si c'est lent, on est sur le serveur de dev, pas de prod.",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "Là je suis en train de bosser dessus... Je mets ça en ligne pour demain, 11h30 !",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "C'est une exception de bon fonctionnement.",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "Personne ne m’a demandé combien de temps cela devait prendre.",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "Tes clients qui sont sous XP ne voient pas le site ? C'est parce qu'ils sont au bureau... c'est pas grave, ils le verront en rentrant chez eux sur leur tablette.",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "C'est la faute du stagiaire.",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "Alors normalement, ça fait pas ça.",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "Je n'ai pas touché à ce module depuis des semaines !",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "C’est sur Mac c’est normal.",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "Si il n'y avait pas d'utilisateur, il n'y aurait pas de bug.",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "Selon moi, ça va toucher seulement 0,02% des utilisateurs.",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "De toute façon, ce message d'erreur ne sert à rien.",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "J'ai pas lu les specs, j'ai une phobie administrative.",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "Pour que les animations soient fluides, il faut scroller tout doucement...",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "Ça vient de ton écran, tu n'as pas la même résolution que moi.",
+        author : "excusesdedev.com"
+    },
+    {
+        quote : "J'ai eu un conflit.",
+        author : "excusesdedev.com"
+    }
+];
